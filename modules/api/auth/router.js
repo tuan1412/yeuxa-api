58e61a7bfabc1215dac6607565185ddbb87fdcbd
hoang anh <hoanganhctm7@gmail.com>
2018-07-03 21:33:10 +0700

    init project

---
+++ modules/api/auth/router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+const authController = require("./controller");
+
+router.post("/", (req, res) => {
+  authController
+    .login(req.body)
+    .then(userInfo => {
+      req.session.userInfo = userInfo;
+      res.send(userInfo);
+    })
+    .catch(error => res.status(error.status).send(error.err));
+});
+
+router.get("/", (req, res) => {
+  if (req.session.userInfo) res.send(req.session.userInfo);
+  else res.send(null);
+});
+
+router.delete("/", (req, res) => {
+  req.session.destroy();
+  res.send("Logged out");
+});
+
+
+
+module.exports = router;
