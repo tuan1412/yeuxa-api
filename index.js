dea03f6c0e67d12bd42f1cd16fdd99890ebda6de
anhph <anhph@atmarkcafe.org>
2018-07-16 17:37:14 +0700

    fix accept friend

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-const config = require("./config-local.json");
-const _ = require("lodash");
-const background = require('./background');
+
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const config = require("./config-local.json");
+const background = require("./background");
 
 const userRouter = require("./modules/api/users/router");
 const authRouter = require("./modules/api/auth/router");
 const friendRouter = require("./modules/api/friend/router");
 const roomRouter = require("./modules/api/room/router");
 
-mongoose.connect(config.mongoPath, err => {
-  if (err) console.error(err);
-  else console.log("Database connect successful");
-});
+mongoose.connect(
+  config.mongoPath,
+  err => {
+    if (err) console.error(err);
+    else console.log("Database connect successful");
+  }
+);
 
 app.use((req, res, next) => {
   res.setHeader("X-Frame-Options", "ALLOWALL");
@@ -50,40 +54,49 @@ const sessionMiddleware = session({
 
 app.use(sessionMiddleware);
 
-let roomsOnline = {};
+const roomsOnline = {};
 
-io.on('connection', (socket) => {
-  socket.on('online', data => {
+io.on("connection", socket => {
+  socket.on("online", data => {
     socket.username = data.username;
     socket.room = data.room;
     socket.join(data.room);
-    const room = data.room;
-    background(data.place.city).then(res => {
-      if (roomsOnline[room] === undefined) {
-        roomsOnline[room] = [{username: data.username, place: background}]
-      } else {
-        roomsOnline[room].push({username: data.username, place: background});
-      }
-      io.in(room).emit('loveOnline', roomsOnline[room]);
-    })
-    .catch(e => {
-      if (roomsOnline[room] === undefined) {
-        roomsOnline[room] = [{username: data.username, place: {}}]
-      } else {
-        roomsOnline[room].push({username: data.username, place: {}});
-      }
-      io.in(room).emit('loveOnline', roomsOnline[room]);
-    })
-  })
-
-  socket.on('message', message => {
-    io.in(socket.room).emit('loveMessage', message);
+    const { room } = data;
+    background(data.place.city)
+      .then(res => {
+        if (roomsOnline[room] === undefined) {
+          roomsOnline[room] = [{ username: data.username, place: background }];
+        } else {
+          roomsOnline[room].push({
+            username: data.username,
+            place: background
+          });
+        }
+        io.in(room).emit("loveOnline", roomsOnline[room]);
+      })
+      .catch(e => {
+        if (roomsOnline[room] === undefined) {
+          roomsOnline[room] = [{ username: data.username, place: {} }];
+        } else {
+          roomsOnline[room].push({ username: data.username, place: {} });
+        }
+        io.in(room).emit("loveOnline", roomsOnline[room]);
+      });
   });
 
-  socket.on('disconnect', () => {
+  socket.on("message", message => {
+    io.in(socket.room).emit("loveMessage", message);
+  });
+
+  socket.on("disconnect", () => {
     if (!roomsOnline[socket.room]) return;
-    roomsOnline[socket.room].splice(roomsOnline[socket.room].findIndex(user => user.username === socket.username), 1);
-    io.in(socket.room).emit('loveOffline');
+    roomsOnline[socket.room].splice(
+      roomsOnline[socket.room].findIndex(
+        user => user.username === socket.username
+      ),
+      1
+    );
+    io.in(socket.room).emit("loveOffline");
   });
 });
 
@@ -95,15 +108,15 @@ app.use("/api/users", userRouter);
 app.use("/api/friend", friendRouter);
 app.use("/api/room", roomRouter);
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-app.get('/', (req, res) => {
-  res.sendFile('./public/index.html');
+app.get("/", (req, res) => {
+  res.sendFile("./public/index.html");
 });
 
 const port = process.env.PORT || 9000;
 
 server.listen(port, err => {
   if (err) console.log(err);
-  console.log("Server started at port " + port);
+  console.log(`Server started at port ${port}`);
 });
